ab32615aa01152406a5443e955de41583f5b6e87
eguadorodrigo <rrodrigocmiranda@gmail.com>
2023-03-28 13:23:03 -0300

    28032023 - Criação de TU - DELETE / PUT / GET - da controller

--- src/test/java/br/com/eguadordorigo/adopet/controller/TutorControllerTest.java
+++ src/test/java/br/com/eguadordorigo/adopet/controller/TutorControllerTest.java
@@ -5,22 +5,22 @@ import br.com.eguadordorigo.adopet.service.TutorService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 
 @ExtendWith(SpringExtension.class)
@@ -37,27 +37,76 @@ class TutorControllerTest {
     private ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 
     @Test
+    @DisplayName("Teste de chamada com sucesso da rota GET para todos os Tutores")
     void deveConsultarTodosOsTutoresComSucesso() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/tutores"))
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/tutores")
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk());
+
+        verify(service, times(1)).listarTodos();
     }
 
     @Test
+    @DisplayName("Teste de chamada com sucesso da rota GET para um Tutor por id")
+    void deveConsultarUmTutorPorIdComSucesso() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/tutores/{id}",1)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        verify(service, times(1)).listarPorId(any());
+    }
+
+    @Test
+    @DisplayName("Teste de chamada com sucesso da rota POST para cadastrar um Tutor")
     void deveCadastrarUmTutorComSucesso() throws Exception {
         mockMvc
                 .perform(MockMvcRequestBuilders
                         .post("/tutores")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(createTutor()))
+                        .content(criarObjetoTutor()))
                 .andExpect(MockMvcResultMatchers.status().isOk());
+        verify(service, times(1)).criar(any());
     }
 
-    private byte[] createTutor() throws JsonProcessingException {
+
+    @Test
+    @DisplayName("Teste de chamada com sucesso da rota PUT para atualizar um Tutor")
+    void deveAtualizarUmTutorComSucesso() throws Exception {
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .put("/tutores")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(criarObjetoTutorParaAtualizar()))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        verify(service, times(1)).atualizar(any());
+    }
+
+    @Test
+    @DisplayName("Teste de chamada com sucesso da rota DELETE para excluir um Tutor")
+    void deveDeletarUmTutorComSucesso() throws Exception {
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .delete("/tutores/{id}", 1)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        verify(service, times(1)).deletar(any());
+    }
+
+
+    private byte[] criarObjetoTutor() throws JsonProcessingException {
         TutorDto tutorDto = new TutorDto();
         tutorDto.setNome("Teste");
         tutorDto.setEmail("teste@teste.com");
         tutorDto.setSenha("Teste123");
         return ow.writeValueAsBytes(tutorDto);
     }
-
+    private byte[] criarObjetoTutorParaAtualizar() throws JsonProcessingException {
+        TutorDto tutorDto = new TutorDto();
+        tutorDto.setId(1l);
+        tutorDto.setNome("Teste");
+        tutorDto.setEmail("teste@teste.com");
+        tutorDto.setSenha("Teste123");
+        return ow.writeValueAsBytes(tutorDto);
+    }
 }
