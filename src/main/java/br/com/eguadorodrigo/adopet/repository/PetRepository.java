77fd84b7719b35a7ca7cb9a03c69b2387e4d0d0d
eguadorodrigo <rrodrigocmiranda@gmail.com>
2023-04-07 00:17:07 -0300

    07042023 - ajustes da camada de repositório

--- src/main/java/br/com/eguadorodrigo/adopet/repository/PetRepository.java
+++ src/main/java/br/com/eguadorodrigo/adopet/repository/PetRepository.java
@@ -1,8 +1,8 @@
 package br.com.eguadorodrigo.adopet.repository;
 
-import br.com.eguadorodrigo.adopet.model.Pets;
+import br.com.eguadorodrigo.adopet.model.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PetRepository extends JpaRepository<Pets, Long> {
+public interface PetRepository extends JpaRepository<Pet, Long> {
 
 }
