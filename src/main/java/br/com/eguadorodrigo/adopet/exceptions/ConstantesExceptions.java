746e0ea14f228091d64eab028633d29484b7fbf5
eguadorodrigo <rrodrigocmiranda@gmail.com>
2023-04-07 00:17:54 -0300

    07042023 - ajustes da classe de constantes

--- src/main/java/br/com/eguadorodrigo/adopet/exceptions/ConstantesExceptions.java
+++ src/main/java/br/com/eguadorodrigo/adopet/exceptions/ConstantesExceptions.java
@@ -3,6 +3,9 @@ package br.com.eguadorodrigo.adopet.exceptions;
 public class ConstantesExceptions {
 
     public final static String USUARIO_NAO_ENCONTRADO = "Usuário não encontrado.";
+
     public static final String USUARIO_EXISTENTE = "Usuário já existe.";
     public static final String USUARIO_INEXISTENTE = "Usuário não existe";
+    public static final String ABRIGO_NAO_ENCONTRADO = "Abrigo não encontrado";
+    public static final String PET_NAO_ENCONTRADO = "Pet não encontrado";
 }
